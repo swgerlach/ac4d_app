581f6a777775ea4737d7888411fbf8995c34d2d5
Scott Gerlach <swgerlach@gmail.com>
2013-01-08 10:56:47 -0600

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ac4dApp::Application.config.session_store :cookie_store, key: '_ac4d_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ac4dApp::Application.config.session_store :active_record_store
